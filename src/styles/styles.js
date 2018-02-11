a79cde47a3828f1b15f6b5993a0524b9df0a51eb
ImportAntonio <aperez2541@gmail.com>
2018-02-10 16:41:17 -0800

    initial commit

---
+++ src/styles/styles.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    logo: {
+      height: 80,
+      marginBottom: 16,
+      width: 80,
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+    modules: {
+      margin: 20,
+    },
+    modulesHeader: {
+      fontSize: 16,
+      marginBottom: 8,
+    },
+    module: {
+      fontSize: 14,
+      marginTop: 4,
+      textAlign: 'center',
+    }
+  });
