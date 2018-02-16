5425ef1c22e36da4d69364fc7f9e865b880cd03f
ImportAntonio <aperez2541@gmail.com>
2018-02-15 23:36:05 -0800

    add forgot password link

--- app/user/screens/types/LoginScreen.js
+++ app/user/screens/types/LoginScreen.js
@@ -3,11 +3,36 @@ import LoginUI  from './../../components/login';
 import { View } from 'react-native';
 
 export default class LoginScreen extends React.Component {
+
+  static navigatorButtons = {
+    rightButtons: [
+      {
+        title: 'Forgot Password?', 
+        id: 'forgotPassword', 
+        buttonColor: 'white', 
+        buttonFontSize: 14, 
+        buttonFontWeight: '600', 
+      }
+    ]
+  };
+
   constructor(props) {
     super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
     this.state = {
       
     };
+  }
+
+  onNavigatorEvent(event) { 
+    if (event.type == 'NavBarButtonPress') { 
+      if (event.id == 'forgotPassword') { 
+        alert('NavBar', 'Edit button pressed');
+      }
+      if (event.id == 'add') {
+        alert('NavBar', 'Add button pressed');
+      }
+    }
   }
 
   goToNextScreen() {
