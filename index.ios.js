5e12721c0fefe7e28ec8c3b1066eff839f542209
antonioperez <aperez2541@gmail.com>
2018-03-13 21:50:27 -0700

    rename files

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,2 @@
 import { AppRegistry } from 'react-native';
-import App from './app/App';
-
+import App from './app/app';
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 import { AppRegistry } from 'react-native';
-import App from './app/App';
+import App from './app/app';
